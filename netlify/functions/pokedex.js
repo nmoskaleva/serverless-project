261eba34cb5174b0bbbf8c9fefc1ca6e11418b91
nmoskaleva <natalia.moskaleva@criipto.com>
2023-09-06 11:57:57 +0200

    Add region to API

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,23 @@
 // const fetch = require('node-fetch');
 
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+// 2 default params are passed with serverless functions!
+// We'll use those for passing a region
+// 1. event 2. context
+// These are variables attached to serverless functions that we get information on. We can then use them inside our serverless functions.
+
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/` + eventBody.region;
+
+  // this will pass the body of our request, in this case the region. And the method too.
+  console.log(event);
+
   const response = await fetch(POKE_API);
   const data = await response.json();
 
   // every serverless function returns an object
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({ pokemon: data.pokemon_entries }),
   };
 };
